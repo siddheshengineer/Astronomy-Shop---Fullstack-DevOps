cb8c98cb0caf6e5bfc4bde5af757fdcc3634b840
siddhesh-its <siddheshnikam8@gmail.com>
2025-05-10 12:30:15 -0400

    added LKE terraform default files

---
+++ terrafrom/akami/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "2.7.1"
+    }
+  }
+}
+//Use the Linode Provider
+provider "linode" {
+  token = var.token
+}
+
+//Use the linode_lke_cluster resource to create
+//a Kubernetes cluster
+resource "linode_lke_cluster" "astronomy_cluster" {
+    k8s_version = var.k8s_version
+    label = var.label
+    region = var.region
+    tags = var.tags
+
+    dynamic "pool" {
+        for_each = var.pools
+        content {
+            type  = pool.value["type"]
+            count = pool.value["count"]
+        }
+    }
+}
+
+//Export this cluster's attributes
+output "kubeconfig" {
+  value = linode_lke_cluster.foobar.kubeconfig
+  sensitive = true
+}
+
+output "api_endpoints" {
+  value = linode_lke_cluster.astronomy_cluster.api_endpoints
+}
+
+output "status" {
+  value = linode_lke_cluster.astronomy_cluster.status
+}
+
+output "id" {
+  value = linode_lke_cluster.astronomy_cluster.id
+}
+
+output "pool" {
+  value = linode_lke_cluster.astronomy_cluster.pool
+}
---
+++ terrafrom/akami/terraform.tfvars
@@ -0,0 +1,9 @@
+label = "astronomy-lke-cluster"
+k8s_version = "1.32"
+region = "ca-central"
+pools = [
+  {
+   // type : "g6-standard-2"
+    count : 2
+  }
+]
